4d34cad90fca43e9ecac5d4caad2a381ab39c210
Ronald <nottinsonstephen@gmail.com>
2023-07-25 05:25:45 -0700

    0-fizzbuzz.py

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -2,35 +2,38 @@
 """
 Creating Class FizzBuzz
 """
-import sys
 
 
 def fizzbuzz(n):
     """
     The FizzBuzz function generates a sequence of numbers from 1 to n
-
-    where each number is separated by a space. However, for multiples
-    of three, it prints "Fizz" instead of the number, and for multiples
-    of five, it prints "Buzz". If a number is a multiple of both 
-    three and five, it prints "FizzBuzz".
+    where each number is separated by a space.
+    - multiples of three, it prints "Fizz" instead of the number.
+    - for multiples of five, it prints "Buzz". If a number is a
+    multiple of both three and five, it prints "FizzBuzz".
     """
-    for data in range(1, n + 1):
-        if data % 15 == 0:
+    if n < 1:
+        return
+
+    for i in range(1, n + 1):
+        if i % 15 == 0:
             print("FizzBuzz", end=" ")
-        elif data % 3 == 0:
+        elif i % 3 == 0:
             print("Fizz", end=" ")
-        elif data % 5 == 0:
+        elif i % 5 == 0:
             print("Buzz", end=" ")
         else:
-            print(data, end=" ")
+            print(i, end=" ")
 
-    if __name__ == "__main__":
-        if len(sys.argv) != 2:
-            print("Usage: ./0-fizzbuzz.py <n>")
-            sys.exit(1)
 
-        try:
-            n = int(sys.argv[1])
-            fizzbuzz(n)
-        except ValueError:
-            print("Invalid input. Please enter an integer.")
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: ./0-fizzbuzz.py <number>")
+        sys.exit(1)
+
+    try:
+        number = int(sys.argv[1])
+        fizzbuzz(number)
+    except ValueError:
+        print("Invalid input. Please enter an integer.")
